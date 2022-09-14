ef1153eb1c620cae932a3c9f3928deb573a44882
sethhi2 <sethcram@gmail.com>
2022-09-13 20:55:19 -0600

    timing both algs till x solutions found

--- EightQueensProject/PartA/EightQueensMain.py
+++ EightQueensProject/PartA/EightQueensMain.py
@@ -7,7 +7,8 @@ Instructions:
 """
 
 import time
-start_time = time.time()
+
+DESIRED_SOLUTIONS = 5
 
 #import all functs from 8 queens functs
 from EightQueensFuncts import *
@@ -25,13 +26,13 @@ CHILDREN_PER_ITERATION = 2 #same as number of replacements per iteration
 #init space for arrays
 
 populationFitness = [None] * POPULATION_SIZE
-
+elapsedTimeToFindSol = numpy.empty( DESIRED_SOLUTIONS, dtype=float)
 
 worstFitnessData = numpy.empty( EVOLVE_ITERATIONS )
 bestFitnessData = numpy.empty( EVOLVE_ITERATIONS )
 avgFitnessData = numpy.empty( EVOLVE_ITERATIONS )
 
-runsToFindSol = 0
+showFitnessData = False
 
 #region test eval of fitness
 """
@@ -58,101 +59,117 @@ individual[7] = (7,7)
 indivFitness = EvalFitness(individual) #expecting 26 (queens behind other queens shouldn't be threatened)
 """
 #endregion test eval of fitness
-
-#create new and scrap old evo comp rslts if sol not found
-while(True):
-    runsToFindSol += 1
-    
-    #create new population
-    population = CreatePopulation(POPULATION_SIZE, NUMBER_OF_TRAITS, BOARD_SIZE_X, BOARD_SIZE_Y)
-
-    #run for desired evolution iterations
-    for j in range(0, EVOLVE_ITERATIONS ):
-
-        #walk thru each individual in pop
-        for i in range(0, POPULATION_SIZE):
-            #store individual w/ their fitness data
-            populationFitness[i] = PopulationFitness( population[i], EvalFitness(population[i]) )
+for k in range(0, DESIRED_SOLUTIONS):
+    runsToFindSol = 0
+    start_time = time.time()
+    #create new and scrap old evo comp rslts if sol not found
+    while(True):
+        runsToFindSol += 1
         
-        #display pop-fitness before sorting
-        #print(populationFitness)
+        #create new population
+        population = CreatePopulation(POPULATION_SIZE, NUMBER_OF_TRAITS, BOARD_SIZE_X, BOARD_SIZE_Y)
 
-        #sort in ascending order by fitness (low/good to high/bad)
-        populationFitness.sort(key=getFitness)
+        #run for desired evolution iterations
+        for j in range(0, EVOLVE_ITERATIONS ):
 
-        #print(populationFitness)
-
-        #copy sorted pop fitness data to reorder pop
-        for i in range(0, POPULATION_SIZE):
-            population[i] = populationFitness[i].individual
-
-        worstFitnessData[j] = max( populationFitness, key=getFitness ).fitness
-        bestFitnessData[j] = min( populationFitness, key=getFitness ).fitness
-
-        #find avg
-        fitnessSum = 0
-        for i in range(0, POPULATION_SIZE):
-            fitnessSum += populationFitness[i].fitness
-        avgFitnessData[j] = int( fitnessSum/POPULATION_SIZE )
-
-        #if first iteration 
-        #if( j == 0 ):
-            #select 2 parents from pop + show distr graph
-        #    parents = BreedSelection(population, displayDistributionGraph=True)
-        #else:
-            #select 2 parents from pop
-        parents = BreedSelection(population)
-
-        #crossover breed parents to get children
-        children = CrossoverBreed(parents[0], parents[1])
-
-        #create possibly mutated children
-        for child in children:
-            #mutate child 
-            Mutate(child)
+            #walk thru each individual in pop
+            for i in range(0, POPULATION_SIZE):
+                #store individual w/ their fitness data
+                populationFitness[i] = PopulationFitness( population[i], EvalFitness(population[i]) )
             
-        SurvivalReplacement(population, children)
-        
-        #print("asdfs %d" % j)
-        
-        if( bestFitnessData[j] == 0 ):
-            print("Best fitness of zero reached for configuration " + str( populationFitness ) )
-    
-    print("run " + str(runsToFindSol) + " resulted in a best fitness of " + str(bestFitnessData[EVOLVE_ITERATIONS-1]))
-    
-    #if zero fitness reached so sol found
-    if( bestFitnessData[EVOLVE_ITERATIONS-1] == 0 ):
-        print("it took " + str(runsToFindSol) + " runs to find a solution")
-        #exit loop
-        break
-        
-print("My program took", time.time() - start_time, "seconds to run")
+            #display pop-fitness before sorting
+            #print(populationFitness)
 
-t = numpy.arange(0, EVOLVE_ITERATIONS)
+            #sort in ascending order by fitness (low/good to high/bad)
+            populationFitness.sort(key=getFitness)
+
+            #print(populationFitness)
+
+            #copy sorted pop fitness data to reorder pop
+            for i in range(0, POPULATION_SIZE):
+                population[i] = populationFitness[i].individual
+
+            worstFitnessData[j] = max( populationFitness, key=getFitness ).fitness
+            bestFitnessData[j] = min( populationFitness, key=getFitness ).fitness
+
+            #find avg
+            fitnessSum = 0
+            for i in range(0, POPULATION_SIZE):
+                fitnessSum += populationFitness[i].fitness
+            avgFitnessData[j] = int( fitnessSum/POPULATION_SIZE )
+
+            #if first iteration 
+            #if( j == 0 ):
+                #select 2 parents from pop + show distr graph
+            #    parents = BreedSelection(population, displayDistributionGraph=True)
+            #else:
+                #select 2 parents from pop
+            parents = BreedSelection(population)
+
+            #crossover breed parents to get children
+            children = CrossoverBreed(parents[0], parents[1])
+
+            #create possibly mutated children
+            for child in children:
+                #mutate child 
+                Mutate(child)
+                
+            SurvivalReplacement(population, children)
+            
+            #print("asdfs %d" % j)
+            
+            #if( bestFitnessData[j] == 0 ):
+            #    print("Best fitness of zero reached for configuration " + str( populationFitness ) )
+        
+        #print("run " + str(runsToFindSol) + " resulted in a best fitness of " + str(bestFitnessData[EVOLVE_ITERATIONS-1]))
+        
+        #if zero fitness reached so sol found
+        if( bestFitnessData[EVOLVE_ITERATIONS-1] == 0 ):
+            print("it took " + str(runsToFindSol) + " runs to find a solution")
+            #exit loop
+            break
+      
+    elapsedTimeToFindSol[k] = time.time() - start_time
+    
+    #print("My program took", elapsedTimeToFindSol[k], "seconds to run")
+    
+    if(showFitnessData):
+        t = numpy.arange(0, EVOLVE_ITERATIONS)
+        
+        plt.rcParams.update({'font.size': 22})
+        plt.plot(t, bestFitnessData) 
+        plt.grid() #add a grid to graph
+        plt.title('Best Fitness per Iteration')
+        plt.ylabel('Best Fitness')
+        plt.xlabel('Iteration')
+        plt.show()
+
+        #plt.subplot(3, 1, 2)
+        plt.plot(t, avgFitnessData) 
+        plt.grid() #add a grid to graph
+        plt.title('Average Fitness per Iteration')
+        plt.ylabel('Average Fitness')
+        plt.xlabel('Iteration')
+        plt.show()
+
+        #plt.subplot(3, 1, 3)
+        plt.plot(t, worstFitnessData) 
+        plt.grid() #add a grid to graph
+        plt.title('Worst Fitness per Iteration')
+        plt.ylabel('Worst Fitness')
+        plt.xlabel('Iteration')
+        plt.show()
+
+t1 = numpy.arange(0, DESIRED_SOLUTIONS)
 
 #plots:
 plt.rcParams.update({'font.size': 22})
+
 plt.figure(figsize = (10,15)) #1st arg = horizontal space, 2nd arg = vertical space
 #plt.subplot(3, 1, 1)
-plt.plot(t, bestFitnessData) 
+plt.plot(t1, elapsedTimeToFindSol) 
 plt.grid() #add a grid to graph
-plt.title('Best Fitness per Iteration')
-plt.ylabel('Best Fitness')
-plt.xlabel('Iteration')
-plt.show()
-
-#plt.subplot(3, 1, 2)
-plt.plot(t, avgFitnessData) 
-plt.grid() #add a grid to graph
-plt.title('Average Fitness per Iteration')
-plt.ylabel('Average Fitness')
-plt.xlabel('Iteration')
-plt.show()
-
-#plt.subplot(3, 1, 3)
-plt.plot(t, worstFitnessData) 
-plt.grid() #add a grid to graph
-plt.title('Worst Fitness per Iteration')
-plt.ylabel('Worst Fitness')
-plt.xlabel('Iteration')
+plt.title('Elapsed Time per Solution')
+plt.ylabel('Elapsed Time (s)')
+plt.xlabel('Solution Number')
 plt.show()
--- EightQueensProject/PartB/EightQueensMain.py
+++ EightQueensProject/PartB/EightQueensMain.py
@@ -8,7 +8,6 @@ Instructions:
 """
 
 import time
-start_time = time.time()
 
 #import all functs from 8 queens functs
 from EightQueensFuncts import *
@@ -23,17 +22,20 @@ BOARD_SIZE_Y = 8
 EVOLVE_ITERATIONS = 1000
 CHILDREN_PER_ITERATION = 2 #same as number of replacements per iteration
 POSSIBLE_SOLUTIONS = 92
+DESIRED_SOLUTIONS = 5
     
 #init space for arrays
 
 populationFitness = [None] * POPULATION_SIZE
-
+#populationHistory = numpy.empty( EVOLVE_ITERATIONS, dtype=numpy.ndarray )
+#phList = numpy.empty( NUMBER_OF_TRAITS, dtype=numpy.ndarray )
+elapsedTimeToFindSol = numpy.empty( DESIRED_SOLUTIONS, dtype=float)
 
 worstFitnessData = numpy.empty( EVOLVE_ITERATIONS, dtype=int )
 bestFitnessData = numpy.empty( EVOLVE_ITERATIONS, dtype=int )
 avgFitnessData = numpy.empty( EVOLVE_ITERATIONS, dtype=int )
 
-runsToFindSol = 0
+showFitnessData = False
 
 #region test eval of fitness
 """#init arr w/ None
@@ -60,94 +62,129 @@ indivFitness = EvalFitness(individual) #expecting 26 (queens behind other queens
 """
 #endregion test eval of fitness
 
-#create new and scrap old evo comp rslts if sol not found
-while(True):
-    runsToFindSol += 1
-    
-    #create new population
-    population = CreatePopulation(POPULATION_SIZE, BOARD_SIZE_X, BOARD_SIZE_Y)
-    
-    #walk thru each individual in pop
-    for i in range(0, POPULATION_SIZE):
-        #store individual w/ their fitness data
-        populationFitness[i] = IndividualFitness( population[i], EvalFitness(population[i]) )
+for k in range(0, DESIRED_SOLUTIONS):
+    runsToFindSol = 0
+    start_time = time.time()
+
+    #create new and scrap old evo comp rslts if sol not found
+    while(True):
+        runsToFindSol += 1
         
-    #sort in ascending order by fitness (low/good to high/bad)
-    populationFitness.sort(key=getFitness)
-
-    #run for desired evolution iterations
-    for j in range(0, EVOLVE_ITERATIONS ):
-
-        #print(populationFitness)
-
-        worstFitnessData[j] = max( populationFitness, key=getFitness ).fitness
-        bestFitnessData[j] = min( populationFitness, key=getFitness ).fitness
-
-        #find avg
-        fitnessSum = 0
+        #create new population
+        population = CreatePopulation(POPULATION_SIZE, BOARD_SIZE_X, BOARD_SIZE_Y)
+        
+        #walk thru each individual in pop
         for i in range(0, POPULATION_SIZE):
-            fitnessSum += populationFitness[i].fitness
-        avgFitnessData[j] = int( fitnessSum/POPULATION_SIZE )
-
-        #if first iteration 
-        #if( j == 0 ):
-            #select 2 parents from pop + show distr graph
-        #    parents = BreedSelection(populationFitness, displayDistributionGraph=True)
-        #else:
-            #select 2 parents from pop
-        parents = BreedSelection(populationFitness)
-
-        #crossover breed parents to get children
-        children = CrossoverBreed(parents[0].individual, parents[1].individual)
-
-        #create possibly mutated children
-        for child in children:
-            #mutate child 
-            Mutate(child)
+            #store individual w/ their fitness data
+            populationFitness[i] = IndividualFitness( population[i], EvalFitness(population[i]) )
             
-        SurvivalReplacement(populationFitness, children)
-        
-        #print("asdfs %d" % j)
-        
-        #if( bestFitnessData[j] == 0 ):
-        #    print("Best fitness of zero reached for configuration " + str( populationFitness ) )
-    
-    print("run " + str(runsToFindSol) + " resulted in a best fitness of " + str(bestFitnessData[EVOLVE_ITERATIONS-1]))
-    
-    #if zero fitness reached so sol found
-    if( bestFitnessData[EVOLVE_ITERATIONS-1] == 0 ):
-        print("it took " + str(runsToFindSol) + " runs to find a solution")
-        #exit loop
-        break
-        
+        #sort in ascending order by fitness (low/good to high/bad)
+        populationFitness.sort(key=getFitness)
 
-print("My program took", time.time() - start_time, "seconds to run")
+        #run for desired evolution iterations
+        for j in range(0, EVOLVE_ITERATIONS ):
 
-t = numpy.arange(0, EVOLVE_ITERATIONS)
+            #print(populationFitness)
+
+            worstFitnessData[j] = max( populationFitness, key=getFitness ).fitness
+            bestFitnessData[j] = min( populationFitness, key=getFitness ).fitness
+
+            #find avg
+            fitnessSum = 0
+            for i in range(0, POPULATION_SIZE):
+                #take the fitness sum
+                fitnessSum += populationFitness[i].fitness            
+            avgFitnessData[j] = int( fitnessSum/POPULATION_SIZE )
+            
+            #attempt at trait tracking
+            
+            #store pop every evolution iteration
+            #populationHistory[j] = population
+            #phList[:] = population
+            
+            #for i in range(0, NUMBER_OF_TRAITS):
+                #phList[j][i] = []
+                #copy over each individual's i'th col into this iteration's phList (each list is now a col instead of a row)
+            #    phList[i] = [individual[i] for individual in populationHistory[j]]
+                #bleh = [individual[i] for individual in populationHistory[j]]
+            #populationHistory[j] = phList[j]
+            
+            #plt.hist(phList[0], bins = NUMBER_OF_TRAITS)
+            #plt.show()
+
+            #if first iteration 
+            #if( j == 0 ):
+                #select 2 parents from pop + show distr graph
+            #    parents = BreedSelection(populationFitness, displayDistributionGraph=True)
+            #else:
+                #select 2 parents from pop
+            parents = BreedSelection(populationFitness)
+
+            #crossover breed parents to get children
+            children = CrossoverBreed(parents[0].individual, parents[1].individual)
+
+            #create possibly mutated children
+            for child in children:
+                #mutate child 
+                Mutate(child)
+                
+            SurvivalReplacement(populationFitness, children)
+            
+            #print("asdfs %d" % j)
+            
+            #if( bestFitnessData[j] == 0 ):
+            #    print("Best fitness of zero reached for configuration " + str( populationFitness ) )
+        
+        #print("run " + str(runsToFindSol) + " resulted in a best fitness of " + str(bestFitnessData[EVOLVE_ITERATIONS-1]))
+        
+        #if zero fitness reached so sol found
+        if( bestFitnessData[EVOLVE_ITERATIONS-1] == 0 ):
+            print("it took " + str(runsToFindSol) + " runs to find a solution")
+            #exit loop
+            break
+        
+    elapsedTimeToFindSol[k] = time.time() - start_time
+    
+    #print("My program took", elapsedTimeToFindSol[k], "seconds to run")
+    
+    if(showFitnessData):
+        t = numpy.arange(0, EVOLVE_ITERATIONS)
+        
+        plt.rcParams.update({'font.size': 22})
+        plt.plot(t, bestFitnessData) 
+        plt.grid() #add a grid to graph
+        plt.title('Best Fitness per Iteration')
+        plt.ylabel('Best Fitness')
+        plt.xlabel('Iteration')
+        plt.show()
+
+        #plt.subplot(3, 1, 2)
+        plt.plot(t, avgFitnessData) 
+        plt.grid() #add a grid to graph
+        plt.title('Average Fitness per Iteration')
+        plt.ylabel('Average Fitness')
+        plt.xlabel('Iteration')
+        plt.show()
+
+        #plt.subplot(3, 1, 3)
+        plt.plot(t, worstFitnessData) 
+        plt.grid() #add a grid to graph
+        plt.title('Worst Fitness per Iteration')
+        plt.ylabel('Worst Fitness')
+        plt.xlabel('Iteration')
+        plt.show()
+
+t1 = numpy.arange(0, DESIRED_SOLUTIONS)
 
 #plots:
 plt.rcParams.update({'font.size': 22})
+
 plt.figure(figsize = (10,15)) #1st arg = horizontal space, 2nd arg = vertical space
 #plt.subplot(3, 1, 1)
-plt.plot(t, bestFitnessData) 
+plt.plot(t1, elapsedTimeToFindSol) 
 plt.grid() #add a grid to graph
-plt.title('Best Fitness per Iteration')
-plt.ylabel('Best Fitness')
-plt.xlabel('Iteration')
+plt.title('Elapsed Time per Solution')
+plt.ylabel('Elapsed Time (s)')
+plt.xlabel('Solution Number')
 plt.show()
 
-#plt.subplot(3, 1, 2)
-plt.plot(t, avgFitnessData) 
-plt.grid() #add a grid to graph
-plt.title('Average Fitness per Iteration')
-plt.ylabel('Average Fitness')
-plt.xlabel('Iteration')
-plt.show()
-
-#plt.subplot(3, 1, 3)
-plt.plot(t, worstFitnessData) 
-plt.grid() #add a grid to graph
-plt.title('Worst Fitness per Iteration')
-plt.ylabel('Worst Fitness')
-plt.xlabel('Iteration')
-plt.show()
