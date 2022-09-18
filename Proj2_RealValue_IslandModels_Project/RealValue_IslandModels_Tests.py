5cefc9f6ea2634f0820b4ca3ba4a436f9df86042
sethhi2 <sethcram@gmail.com>
2022-09-18 15:31:08 -0600

    type check added for traits

--- Proj2_RealValue_IslandModels_Project/RealValue_IslandModels_Tests.py
+++ Proj2_RealValue_IslandModels_Project/RealValue_IslandModels_Tests.py
@@ -8,13 +8,14 @@ class TestStringMethods(unittest.TestCase):
     def test_IndividualCreation(self):
         """
         Tests individual creation function once for every function.
-        Ensures each generated trait is within the function's domain.
+        Ensures each generated trait is a 64 bit numpy float 
+         and within the function's domain .
         """
         
         #for function in GA_Functions:
         
-        #walk thru funct bounds
-        for functionEnum, functionBounds in functionBoundsDict:
+        #walk thru funct bounds dict
+        for functionEnum, functionBounds in functionBoundsDict.items():
             
             #store bounds
             lower_bound, upper_bound = functionBounds
@@ -28,9 +29,16 @@ class TestStringMethods(unittest.TestCase):
             
             #walk thru individual's traits
             for testTrait in testIndividual:
+                #bounds check
                 self.assertTrue( 
                     testTrait >= lower_bound and testTrait <= upper_bound, 
-                    "Test trait for {} is out of bounds.".format(functionEnum) 
+                    "A test trait for {} is out of bounds.".format(functionEnum) 
+                )
+                #type check
+                testTraitType = type(testTrait)
+                self.assertTrue(
+                    testTraitType == numpy.float64,
+                    "A test trait for {} isn't a float. It's a {}.".format(functionEnum, testTraitType) 
                 )
 
     def test_PopulationCreation(self):
